71fb36eb69af1a8ec9d4a60344dfb26a67b5b0ae
uristrauss <uriel.strauss@gmail.com>
2023-07-12 09:03:43 -0300

    swagger autenticacion

--- src/Controller/authentificationController.js
+++ src/Controller/authentificationController.js
@@ -3,6 +3,40 @@ import { Authenticate } from '../common/jwt.strategy.js';
 import { getSignedToken } from "../services/authentification.js";
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Autentificacion:
+ *       type: object
+ */
+
+
+/**
+ * @swagger
+ * tags:
+ *  name: Autentificacion
+ *  description: El token de autenticación.
+ */
+
+/**
+ * @swagger
+ * /authentification:
+ *   get:
+ *     summary: Devuelve un bearer token para la autentificacion.
+ *     tags: [Autentificacion]
+ *     responses:
+ *       '200':
+ *         description: El token de autenticación.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Pelicula'
+ */
+
+
 router.get('/login', async (req,res)  =>{ 
     const token = await getSignedToken();
     return res.status(200).send(token);
